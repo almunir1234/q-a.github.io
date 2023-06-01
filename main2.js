d38b1c0a047d5756cc9c38832476f5d3084067ff
almunir1234 <135009193+almunir1234@users.noreply.github.com>
2023-06-01 23:35:13 +0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,13 @@
+
+function Save() {
+
+    var JB1 = document.getElementById('op').value;
+    if(JB1 == 1){
+        window.alert("I Love You To Ka");
+    }
+
+    else {
+        window.alert("eummm, but I Will Still Love You hehehe");
+    }
+}
+   
